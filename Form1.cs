8e549799718d173c5b7d4e7746250c522de99f7f
salah-sorour-dev <soroursalaheddine@gmail.com>
2026-06-26 11:22:33 +0100

    Make one Event instead of 9 events

--- Form1.cs
+++ Form1.cs
@@ -176,51 +176,12 @@ namespace WindowsFormsApp1
 
         }
 
-        private void button1_Click(object se7nder, EventArgs e)
-        {
-            ChangeImage(button1);
-        }
 
-        private void button2_Click(object sender, EventArgs e)
+        private void button_Click(object sender, EventArgs e)
         {
-            ChangeImage(button2);
+            ChangeImage((Button) sender);
         }
-
-        private void button3_Click(object sender, EventArgs e)
-        {
-            ChangeImage(button3);
-        }
-
-        private void button4_Click(object sender, EventArgs e)
-        {
-            ChangeImage(button4);
-        }
-
-        private void button5_Click(object sender, EventArgs e)
-        {
-            ChangeImage(button5);
-        }
-
-        private void button6_Click(object sender, EventArgs e)
-        {
-            ChangeImage(button6);
-        }
-
-        private void button7_Click(object sender, EventArgs e)
-        {
-            ChangeImage(button7);
-        }
-
-        private void button8_Click(object sender, EventArgs e)
-        {
-            ChangeImage(button8);
-        }
-
-        private void button9_Click(object sender, EventArgs e)
-        {
-            ChangeImage(button9);
-        }
-
+        
 
         public void ChangeImage(Button btn)
         {
